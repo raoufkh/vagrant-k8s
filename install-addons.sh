868830ca5ac5b3f28b6325d1e2bb199f5b394394
raoufkh <khich.abderaouf@gmail.com>
2023-03-18 10:21:55 +0100

    update multus install

--- install-addons.sh
+++ install-addons.sh
@@ -4,7 +4,7 @@ echo "Install a CNI plugin (Flannel) to the cluster"
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
 echo "Install Multus"
-kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick-plugin.yml
+kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml
 
 echo "Install Metrics Server"
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
