c6dc79fc5d990b4902c9b60bade3ae0a4297d02b
raoufkh <khich.abderaouf@gmail.com>
2023-03-20 11:47:57 +0100

    update github token

--- clone-projects.sh
+++ clone-projects.sh
@@ -3,9 +3,9 @@
 echo "Clone projects in $pwd"
 
 cd ~
-git clone https://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:x-oauth-basic@github.com/towoards5gs/t5gs-helm.git
-git clone https://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:x-oauth-basic@github.com/raoufkh/5gc-observer-perf-evaluation.git
-git clone https://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:x-oauth-basic@github.com/raoufkh/ml-observability-5g.git
+git clone https://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:x-oauth-basic@github.com/towoards5gs/t5gs-helm.git
+git clone https://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:x-oauth-basic@github.com/raoufkh/5gc-observer-perf-evaluation.git
+git clone https://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:x-oauth-basic@github.com/raoufkh/ml-observability-5g.git
 
 
 
