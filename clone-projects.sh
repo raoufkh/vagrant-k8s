de0520b5056bae90942f98ddadee9519a46cc5e7
raoufkh <khich.abderaouf@gmail.com>
2023-03-21 13:33:42 +0100

    sync

--- clone-projects.sh
+++ clone-projects.sh
@@ -3,9 +3,10 @@
 echo "Clone projects in $pwd"
 
 cd ~
-git clone https://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:x-oauth-basic@github.com/towoards5gs/t5gs-helm.git
-git clone https://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:x-oauth-basic@github.com/raoufkh/5gc-observer-perf-evaluation.git
-git clone https://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:x-oauth-basic@github.com/raoufkh/ml-observability-5g.git
+git clone https://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:x-oauth-basic@github.com/towoards5gs/fiveg-trafficmeter.git && \
+  git clone https://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:x-oauth-basic@github.com/towoards5gs/t5gs-helm.git && \
+  git clone https://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:x-oauth-basic@github.com/raoufkh/5gc-observer-perf-evaluation.git && \
+  git clone https://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:x-oauth-basic@github.com/raoufkh/ml-observability-5g.git
 
 
 
