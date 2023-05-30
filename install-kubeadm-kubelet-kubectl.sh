fcbb7e5572d9e93d112e4ba254a808772b1e955e
raoufkh <khich.abderaouf@gmail.com>
2023-05-31 01:51:12 +0200

    update install of k8s
    Signed-off-by: raoufkh <khich.abderaouf@gmail.com>

--- install-kubeadm-kubelet-kubectl.sh
+++ install-kubeadm-kubelet-kubectl.sh
@@ -3,10 +3,10 @@
 # KUBE_VERSION=1.20.11-00
 
 echo "Install kubeadm, kubelet, kubectl"
-sudo apt-get -y update 
+sudo apt-get -y update
 sudo apt-get install -y apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get -y install kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
